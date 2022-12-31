5c1bc2a59847a1822fb30e608a6b5c75515594e3
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2022-12-31 19:21:50 +0200

    allow build script to run everywhere

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-zip \
-  -r extension.zip . \
-  -x scripts/\* \
-  -x images/before.png \
-  -x images/after.png \
-  -x .idea/\* \
-  -x .gitignore \
-  -x .git/\* \
-  -x README.md
+ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
+echo "Script dir: $SCRIPT_DIR"
+
+(
+  cd "$ROOT_DIR" && \
+  ( rm "./extension.zip" || true) && \
+  zip \
+    -r extension.zip . \
+    -x scripts/\* \
+    -x images/before.png \
+    -x images/after.png \
+    -x .idea/\* \
+    -x .gitignore \
+    -x .git/\* \
+    -x README.md \
+    -x .DS_Store
+)
